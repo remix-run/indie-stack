c3027f6bf17397c527d099f0de086c33689265c6
Kent C. Dodds <me+github@kentcdodds.com>
2022-03-10 17:00:06 -0700

    keep prisma out of non-models code

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,6 +1,8 @@
 import bcrypt from "@node-rs/bcrypt";
 import { prisma } from "~/db.server";
 
+export type { User } from "@prisma/client";
+
 export async function getUserById(id: string) {
   return prisma.user.findUnique({ where: { id } });
 }
--- app/session.server.ts
+++ app/session.server.ts
@@ -1,6 +1,6 @@
-import type { User } from "@prisma/client";
 import { createCookieSessionStorage, redirect } from "remix";
 import invariant from "tiny-invariant";
+import type { User } from "./models/user.server";
 import { getUserById } from "./models/user.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
