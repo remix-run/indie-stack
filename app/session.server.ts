40398d90c6635d15fde33b90c5add7b7fcb1bc3e
Michaël De Boey <info@michaeldeboey.be>
2022-03-15 21:39:01 +0100

    chore: use `~` in file path wherever possible (#6)

--- app/session.server.ts
+++ app/session.server.ts
@@ -1,7 +1,8 @@
 import { createCookieSessionStorage, redirect } from "remix";
 import invariant from "tiny-invariant";
-import type { User } from "./models/user.server";
-import { getUserById } from "./models/user.server";
+
+import type { User } from "~/models/user.server";
+import { getUserById } from "~/models/user.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
--- app/utils.ts
+++ app/utils.ts
@@ -1,6 +1,7 @@
-import type { User } from "./models/user.server";
 import { useMemo } from "react";
 import { useMatches } from "remix";
+
+import type { User } from "~/models/user.server";
 
 /**
  * This base hook is used in other hooks to quickly search for specific data
