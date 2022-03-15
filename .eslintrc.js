ba2a7d8d460cf0c549a27bc566169b0e4f792aa8
Kent C. Dodds <me+github@kentcdodds.com>
2022-03-15 10:06:18 -0600

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,17 @@
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "prettier"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/jest",
+    "prettier",
+  ],
+  // we're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but it we have to explicitly
+  // set the jest version.
+  settings: {
+    jest: {
+      version: 27,
+    },
+  },
 };
