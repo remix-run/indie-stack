3da9364625b88a7f6685f19403e5aa67cc987765
Dwight Watson <dwightwatson@users.noreply.github.com>
2022-03-19 05:15:42 +1100

    Remove double space in class name (#16)

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -106,7 +106,7 @@ export default function NewNotePage() {
       <div className="text-right">
         <button
           type="submit"
-          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
         >
           Save
         </button>
