aad919f886004d1ef4272b55a2a9c2a7ecc75aff
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-26 19:59:14 -0700

    feat(vitest): provide a paved path for discovery of tests for *.server.ts files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
+
+const defaultInclude = configDefaults.include ?? [];
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
+    include: [
+      ...defaultInclude,
+      // Provide a test "auto-discovery" convention for tests of files
+      // that follow the Remix `*.server.ts` filename convention
+      "**/__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+    ],
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
