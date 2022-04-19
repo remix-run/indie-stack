ead60df10b306ab35fa51b61224110951a078c8e
Jacob Ebey <jacob.ebey@live.com>
2022-04-19 11:30:20 -0700

    fix: open redirects

--- app/utils.ts
+++ app/utils.ts
@@ -3,6 +3,23 @@ import { useMemo } from "react";
 
 import type { User } from "~/models/user.server";
 
+const DEFAULT_REDIRECT = "/";
+
+export function safeRedirect(
+  to: FormDataEntryValue | string | null | undefined,
+  defaultRedirect: string = DEFAULT_REDIRECT
+) {
+  if (!to || typeof to !== "string") {
+    return defaultRedirect;
+  }
+
+  if (!to.startsWith("/") || to.startsWith("//")) {
+    return defaultRedirect;
+  }
+
+  return to;
+}
+
 /**
  * This base hook is used in other hooks to quickly search for specific data
  * across all loader data using useMatches.
