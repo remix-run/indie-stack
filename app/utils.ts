2e482f2a2a087b98283fabd2c123de3a7402559a
Kent C. Dodds <me+github@kentcdodds.com>
2022-04-20 11:21:52 -0400

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -5,6 +5,13 @@ import type { User } from "~/models/user.server";
 
 const DEFAULT_REDIRECT = "/";
 
+/**
+ * This should be used any time the redirect path is user-provided
+ * (Like the query string on our login/signup pages). This avoids
+ * open-redirect vulnerabilities.
+ * @param {string} to The redirect destination
+ * @param {string} defaultRedirect The redirect to use if the to is unsafe.
+ */
 export function safeRedirect(
   to: FormDataEntryValue | string | null | undefined,
   defaultRedirect: string = DEFAULT_REDIRECT
