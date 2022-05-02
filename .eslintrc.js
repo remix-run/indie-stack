35f704f328b56a3ab48aa3a235c231cd6a0909bf
Tyler Nieman <tyler@tsnieman.net>
2022-05-02 14:04:23 -0600

    chore: add missing word to eslintrc comment (#66)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "prettier",
   ],
   // we're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but it we have to explicitly
+  // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.
   settings: {
     jest: {
