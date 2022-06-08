1d17aff5b25d55e0dd319aece893e27b6af30b85
Logan McAnsh <logan@mcan.sh>
2022-06-08 16:13:40 -0400

    chore: update docker entrypoint so locally created images work the same as on fly

--- Dockerfile
+++ Dockerfile
@@ -58,4 +58,4 @@ COPY --from=build /myapp/package.json /myapp/package.json
 COPY --from=build /myapp/start.sh /myapp/start.sh
 COPY --from=build /myapp/prisma /myapp/prisma
 
-CMD ["npm", "start"]
+ENTRYPOINT [ "./start.sh" ]
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
+#!/bin/sh
+
 # This file is how Fly starts the server (configured in fly.toml). Before starting
 # the server though, we need to run any prisma migrations that haven't yet been
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
-
-#!/bin/sh
 
 set -ex
 npx prisma migrate deploy
