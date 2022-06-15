dd03b2429f1889159f403e5f1137cfd4b3852ed0
Logan McAnsh <logan@mcan.sh>
2022-06-14 21:25:24 -0400

    chore: convert seed script to cjs
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient();
 
