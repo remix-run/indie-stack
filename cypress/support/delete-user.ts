161c1b80c583cf788bc1fa0b05b05c9c5466f258
Logan McAnsh <logan@mcan.sh>
2022-06-29 12:49:12 -0400

    chore: remove installGlobals deep import from @remix-run/node (#116)

--- cypress/support/delete-user.ts
+++ cypress/support/delete-user.ts
@@ -3,7 +3,7 @@
 // npx ts-node --require tsconfig-paths/register ./cypress/support/delete-user.ts username@example.com
 // and that user will get deleted
 
-import { installGlobals } from "@remix-run/node/globals";
+import { installGlobals } from "@remix-run/node";
 import { prisma } from "~/db.server";
 
 installGlobals();
