e8d39cf00d92d2d16abab93025e4b4e47f8c808c
Kent C. Dodds <me+github@kentcdodds.com>
2022-07-13 10:26:09 -0600

    upgrade all dependencies and implement streaming

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,11 @@
-import type { EntryContext } from "@remix-run/node";
+import { PassThrough } from "stream";
+import { renderToPipeableStream } from "react-dom/server";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import { Response } from "@remix-run/node";
+import type { EntryContext, Headers } from "@remix-run/node";
+import isbot from "isbot";
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -8,14 +13,38 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  const callbackName = isbot(request.headers.get("user-agent"))
+    ? "onAllReady"
+    : "onShellReady";
 
-  responseHeaders.set("Content-Type", "text/html");
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        [callbackName]() {
+          let body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+          pipe(body);
+        },
+        onShellError(err: unknown) {
+          reject(err);
+        },
+        onError(error: unknown) {
+          didError = true;
+          console.error(error);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
 }
