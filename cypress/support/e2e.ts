57fc0b37a142488bcc879479fefa2c0597f43418
Kent C. Dodds <me+github@kentcdodds.com>
2022-07-18 11:11:15 -0600

    temporarily workaround cypress issues

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,2 +1,13 @@
 import "@testing-library/cypress/add-commands";
 import "./commands";
+
+beforeEach(() => {
+  cy.on("uncaught:exception", (err) => {
+    // Cypress and React Hydrating the document don't get along
+    // for some unknown reason. Hopefully we figure out why eventually
+    // so we can remove this.
+    if (/hydrat/i.test(err.message)) {
+      return false;
+    }
+  });
+});
