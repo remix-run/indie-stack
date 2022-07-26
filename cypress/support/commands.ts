34a6302fe5c8a80c7221092be61d997bd71b5eba
RichardHpa <richard.m.hpa@gmail.com>
2022-07-27 09:07:38 +1200

    shortend waitTime and added it as a param

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -78,18 +78,18 @@ function deleteUserByEmail(email: string) {
   cy.clearCookie("__session");
 }
 
-Cypress.Commands.add("login", login);
-Cypress.Commands.add("cleanupUser", cleanupUser);
-
 // We're waiting a second because of this issue happen randomly
 // https://github.com/cypress-io/cypress/issues/7306
 // Also added custom types to avoid getting detached
 // https://github.com/cypress-io/cypress/issues/7306#issuecomment-1152752612
 // ===========================================================
-function visitAndCheck(url: string) {
+function visitAndCheck(url: string, waitTime: number = 100) {
   cy.visit(url);
-  cy.location("pathname").should("contain", url).wait(1000);
+  cy.location("pathname").should("contain", url).wait(waitTime);
 }
+
+Cypress.Commands.add("login", login);
+Cypress.Commands.add("cleanupUser", cleanupUser);
 Cypress.Commands.add("visitAndCheck", visitAndCheck);
 
 /*
