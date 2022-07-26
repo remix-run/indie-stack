d3871eb52cb8f31ebd44728b3d2f014e770847e4
RichardHpa <richard.m.hpa@gmail.com>
2022-07-27 10:19:54 +1200

    reverted timout back to 1000

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -34,6 +34,8 @@ declare global {
        * @memberof Chainable
        * @example
        *    cy.visitAndCheck('/')
+       *  @example
+       *    cy.visitAndCheck('/', 500)
        */
       visitAndCheck: typeof visitAndCheck;
     }
@@ -83,7 +85,7 @@ function deleteUserByEmail(email: string) {
 // Also added custom types to avoid getting detached
 // https://github.com/cypress-io/cypress/issues/7306#issuecomment-1152752612
 // ===========================================================
-function visitAndCheck(url: string, waitTime: number = 100) {
+function visitAndCheck(url: string, waitTime: number = 1000) {
   cy.visit(url);
   cy.location("pathname").should("contain", url).wait(waitTime);
 }
