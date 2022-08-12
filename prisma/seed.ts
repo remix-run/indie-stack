7c23ab436bb2a42fec5a126d14ee7151e6ac9e85
Michaël De Boey <info@michaeldeboey.be>
2022-08-12 19:17:07 +0200

    feat: remove all TypeScript references when `isTypeScript` is passed as `false` to `remix.init` (#127)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcryptjs");
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
