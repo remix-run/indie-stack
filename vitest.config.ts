ddbfa53479a99cd946edafc11768ef5d3e9eca0c
Michaël De Boey <info@michaeldeboey.be>
2023-01-18 16:54:37 +0100

    chore: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
