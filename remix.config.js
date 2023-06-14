e5b3f57185ce2df9cc73edef52e320f39e1e437f
Michaël De Boey <info@michaeldeboey.be>
2023-06-14 10:06:58 +0200

    chore: update dependencies (#240)

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   future: {
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
