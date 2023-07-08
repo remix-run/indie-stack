6b88b196ce3bc5c33e259bea844dc2f95c38f0cc
Michaël De Boey <info@michaeldeboey.be>
2023-07-08 15:29:28 +0200

    feat: simplify Prisma setup (#245)

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,23 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+import { singleton } from "./singleton.server";
 
-declare global {
-  var __db__: PrismaClient | undefined;
-}
-
-// This is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-// In production, we'll have a single connection to the DB.
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.__db__) {
-    global.__db__ = new PrismaClient();
-  }
-  prisma = global.__db__;
-  prisma.$connect();
-}
+// Hard-code a unique key, so we can look up the client when this module gets re-imported
+const prisma = singleton("prisma", () => new PrismaClient());
+prisma.$connect();
 
 export { prisma };
