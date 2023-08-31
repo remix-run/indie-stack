102756921761f6ddc45e68d70af8274a1825eb23
Michaël De Boey <info@michaeldeboey.be>
2023-08-31 02:03:49 +0200

    feat: move to ESM

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -110,9 +110,9 @@ module.exports = {
       },
       settings: {
         jest: {
-          // we're using vitest which has a very similar API to jest
-          // (so the linting plugins work nicely), but it means we have to explicitly
-          // set the jest version.
+          // We're using vitest which has a very similar API to jest
+          // (so the linting plugins work nicely), but it means we
+          // have to set the jest version explicitly.
           version: 28,
         },
       },
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
 /** @type {import("prettier").Config} */
-module.exports = {
+export default {
   plugins: ["prettier-plugin-tailwindcss"],
 };
