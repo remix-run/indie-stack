666cef4e59668b7917de0e544c4eb6b6c691b721
krivokhizhin.a <anton.kri97@gmail.com>
2023-09-28 19:56:01 +0300

    use generic types for cy.then and cy.get aliases

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -47,7 +47,7 @@ function login({
 }: {
   email?: string;
 } = {}) {
-  cy.then(() => ({ email })).as("user");
+  cy.then<{ email: string }>(() => ({ email })).as("user");
   cy.exec(
     `npx ts-node --require tsconfig-paths/register ./cypress/support/create-user.ts "${email}"`,
   ).then(({ stdout }) => {
@@ -56,15 +56,14 @@ function login({
       .trim();
     cy.setCookie("__session", cookieValue);
   });
-  return cy.get("@user");
+  return cy.get<{ email?: string }>("@user");
 }
 
 function cleanupUser({ email }: { email?: string } = {}) {
   if (email) {
     deleteUserByEmail(email);
   } else {
-    cy.get("@user").then((user) => {
-      const email = (user as { email?: string }).email;
+    cy.get<{ email?: string }>("@user").then(({ email }) => {
       if (email) {
         deleteUserByEmail(email);
       }
