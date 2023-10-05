e361168403bfb4da9c96eddd23fdb53ef9e3355d
Matt Brophy <matt@brophy.org>
2023-10-05 12:18:42 -0400

    Fix ESM issue

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,8 +1,9 @@
-import { remember } from "@epic-web/remember";
 import { PrismaClient } from "@prisma/client";
 
+import { singleton } from "./singleton.server";
+
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-const prisma = remember("prisma", () => new PrismaClient());
+const prisma = singleton("prisma", () => new PrismaClient());
 prisma.$connect();
 
 export { prisma };
