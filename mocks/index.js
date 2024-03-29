1a9e9a7e9ec9737d24943a86bbb19a6093e258d3
Michaël De Boey <info@michaeldeboey.be>
2024-03-29 15:08:41 +0100

    chore: update dependencies (#275)

--- mocks/index.js
+++ mocks/index.js
@@ -1,11 +1,9 @@
-const { rest } = require("msw");
+const { http, passthrough } = require("msw");
 const { setupServer } = require("msw/node");
 
 // put one-off handlers that don't really need an entire file to themselves here
 const miscHandlers = [
-  rest.post(`${process.env.REMIX_DEV_HTTP_ORIGIN}/ping`, (req) =>
-    req.passthrough(),
-  ),
+  http.post(`${process.env.REMIX_DEV_HTTP_ORIGIN}/ping`, () => passthrough()),
 ];
 
 const server = setupServer(...miscHandlers);
