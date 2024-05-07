03583a2732c06739a7ea26318ea459ca96910225
Shmuli Margulies <shmuli9@gmaail.com>
2024-05-07 13:01:25 +0100

    replaced ts-node with vite-node to simplify running esm scripts

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -1,6 +1,6 @@
 // Use this to create a new user and login with that user
 // Simply call this with:
-// npx ts-node -r tsconfig-paths/register ./cypress/support/create-user.ts username@example.com,
+// npx vite-node ./cypress/support/create-user.ts username@example.com,
 // and it will log out the cookie value you can use to interact with the server
 // as that new user.
 
