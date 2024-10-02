2a1e69e9475792f14a669fc1477009eefa68ef97
Michaël De Boey <info@michaeldeboey.be>
2024-10-03 00:10:31 +0200

    feat: use `tsx` (#298)

--- remix.init/index.js
+++ remix.init/index.js
@@ -10,8 +10,8 @@ const semver = require("semver");
 const cleanupCypressFiles = ({ fileEntries, packageManager }) =>
   fileEntries.flatMap(([filePath, content]) => {
     const newContent = content.replace(
-      new RegExp("npx ts-node", "g"),
-      packageManager.name === "bun" ? "bun" : `${packageManager.exec} ts-node`,
+      new RegExp("npx tsx", "g"),
+      packageManager.name === "bun" ? "bun" : `${packageManager.exec} tsx`,
     );
 
     return [fs.writeFile(filePath, newContent)];
@@ -87,13 +87,13 @@ const updatePackageJson = ({ APP_NAME, packageJson, packageManager }) => {
     name: APP_NAME,
     devDependencies:
       packageManager.name === "bun"
-        ? removeUnusedDependencies(devDependencies, ["ts-node"])
+        ? removeUnusedDependencies(devDependencies, ["tsx"])
         : devDependencies,
     prisma: {
       ...prisma,
       seed:
         packageManager.name === "bun"
-          ? prismaSeed.replace("ts-node", "bun")
+          ? prismaSeed.replace("tsx", "bun")
           : prismaSeed,
     },
     scripts,
